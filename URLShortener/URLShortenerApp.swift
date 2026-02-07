f3322dad5594d80efd4115555963f62e1ba62196
Gustavo de Camargo da Silva Ramalho <gustavocsramalho@gmail.com>
2026-02-07 10:46:10 -0300

    Initial Commit

---
+++ URLShortener/URLShortenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerApp.swift
+//  URLShortener
+//
+//  Created by Gustavo Ramalho on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct URLShortenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ URLShortenerTests/URLShortenerTests.swift
@@ -0,0 +1,16 @@
+//
+//  URLShortenerTests.swift
+//  URLShortenerTests
+//
+//  Created by Gustavo Ramalho on 07/02/26.
+//
+
+import Testing
+
+struct URLShortenerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
